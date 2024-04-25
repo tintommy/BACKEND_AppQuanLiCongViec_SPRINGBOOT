c787268c343069af5d2f515e59c1419d63936136
Nguyen Tin <tinnguyen3523@gmail.com>
2024-04-25 17:47:08 +0700

    update

--- src/main/java/org/example/quanlycongviec/security/CustomUserDetailsService.java
+++ src/main/java/org/example/quanlycongviec/security/CustomUserDetailsService.java
@@ -21,30 +21,33 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         NguoiDung temp = nguoiDungRepository.findByEmail(email);
-        if(temp==null){
+//        if(temp==null){
+//            GrantedAuthority authority = new SimpleGrantedAuthority("ABC");
+//            return new User(
+//                    "gmail.com",
+//                    "abc",
+//                    new ArrayList<GrantedAuthority>() {{
+//                        add(authority);
+//                    }}
+//            );
+//        }
+
+        if (temp != null) {
+            NguoiDung user = nguoiDungRepository.findById(temp.getMaNguoiDung())
+                    .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
+
+
             GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+
             return new User(
-                    "abc@gmail.com",
-                    "abc",
+                    email,
+                    user.getMatKhau(),
                     new ArrayList<GrantedAuthority>() {{
                         add(authority);
                     }}
             );
         }
+        throw new UsernameNotFoundException("Could not find email: " + email);
 
-
-         NguoiDung user = nguoiDungRepository.findById(temp.getMaNguoiDung())
-                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
-
-
-        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
-
-        return new User(
-                email,
-                user.getMatKhau(),
-                new ArrayList<GrantedAuthority>() {{
-                    add(authority);
-                }}
-        );
     }
 }
